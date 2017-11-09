2f4f8fbc0b9c079bf539399da161609d40a95f8a
Maxim Petrov <mmrmaximuzz@gmail.com>
2017-11-09 16:40:28 +0300

    reverse - pure c version improved (#15)

--- solutions/1_reverse/c/main.c
+++ solutions/1_reverse/c/main.c
@@ -1,20 +1,49 @@
+#define _POSIX_C_SOURCE 200809L
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#define MAX_SIZE 1000
 
-typedef unsigned int uint;
-
-int main() 
+int main()
 {
-	char buffer[MAX_SIZE] = {' '};
-	char rev[MAX_SIZE] = {' '};
-	uint length = 0;
+    char *line = NULL;
+    size_t block_length = 0;
 
-	scanf("%s", buffer);
-	length = strlen(buffer);
+    /* read the input string */
+    ssize_t n_read = getline(&line, &block_length, stdin);
+    if (n_read == -1) {
+        if (errno != 0) {
+            /* error in getline */
+            perror("error in getline");
+            free(line);
+            return 1;
+        }
+        /* empty string */
+        return 0;
+    }
+    size_t length = strlen(line);
 
-	for (uint i = 0; i < length; ++i)
-		rev[i] = buffer[length - 1 - i];
+    /* check if the line ends with newline character */
+    int ends_with_newline = line[length - 1] == '\n';
+    if (ends_with_newline)
+        --length;
 
-	printf("%s\n", rev);
+    /* single newline symbol check */
+    if (length == 0)
+        return 0;
+
+    /* in-place reverse */
+    for (size_t i = 0; i < length / 2; ++i) {
+        char temp = line[i];
+        line[i] = line[length - i - 1];
+        line[length - i - 1] = temp;
+    }
+
+    /* output with correct newline symbol */
+    printf("%s", line);
+    if (!ends_with_newline)
+        printf("\n");
+
+    free(line);
+    return 0;
 }
